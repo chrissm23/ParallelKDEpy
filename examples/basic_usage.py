6c99dc4b116f185ae79ee5993c309d9f55dffeef
chrissm23 <ccsm983@gmail.com>
2025-08-13 13:39:21 +0200

    Included basic usage examples

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,27 +1,50 @@
 """
-A quick runnable example of using ParallelKDEpy.
+A quick example of using ParallelKDEpy.
 """
-
-# TODO: Mark this directory as runnable in README
 
 import numpy as np
 import matplotlib.pyplot as plt
-from parallelkdepy import estimate_kde
+
+import os
+
+import parallelkdepy as pkde
 
 
 def main():
-    # generate synthetic data
-    data = np.concatenate(
-        [np.random.normal(-2, 0.5, 500), np.random.normal(3, 1.0, 500)]
+    data = np.random.randn(10000, 1)  # Generate random data
+
+    density_estimation = pkde.DensityEstimation(
+        data,
+        grid=True,
+        device="cpu",
     )
-    # estimate KDE
-    grid, density = estimate_kde(data, bandwidth=0.7)
-    # plot result
-    plt.plot(grid, density)
-    plt.title("ParallelKDEpy Estimate")
-    plt.xlabel("x")
-    plt.ylabel("density")
-    plt.show()
+    density_estimation.estimate_density("parallelEstimator")
+
+    density_estimated = density_estimation.get_density()
+    grid_coordinates = density_estimation.generate_grid().to_meshgrid()[0]
+
+    density_true = np.exp(-0.5 * grid_coordinates**2) / np.sqrt(2 * np.pi)
+
+    plt.plot(
+        grid_coordinates, density_true, label="True Density", lw=2, c="cornflowerblue"
+    )
+    plt.plot(
+        grid_coordinates,
+        density_estimated,
+        label="Estimated Density",
+        lw=2,
+        c="firebrick",
+    )
+    plt.xlabel("Random Variable")
+    plt.ylabel("Density")
+    plt.legend()
+    plt.grid()
+
+    plt.savefig(
+        os.path.join(os.path.dirname(__file__), "basic_usage.png"),
+        dpi=300,
+        bbox_inches="tight",
+    )
 
 
 if __name__ == "__main__":
